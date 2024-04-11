e1d738b688cd033f18b75d027d8474cbf5a4084c
dipesh <dipeshmaurya152@gmail.com>
2024-04-11 12:07:05 +0530

    Initial commit

---
+++ lib/Home/Component/Lec 5.4/Calc.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Calcscreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Calcscreen extends StatefulWidget {
+  const Calcscreen({super.key});
+
+  @override
+  State<Calcscreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Calcscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xff54759E),
+        leading: Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+        title: const Text(
+          'Calc',
+          style: TextStyle(
+            color: Colors.white,
+
+            fontWeight: FontWeight.bold,
+
+            // backgroundColor:
+          ),
+        ),
+
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$count',
+            style: const TextStyle(
+              color: Color(0xff9E9E9E),
+              fontSize: 75,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // mainAxisAlignment: MainAxisAlignment.,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      count -= 2;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Color(0xff54759E),
+                      borderRadius: BorderRadiusDirectional.circular(10),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        '-2',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      count += 2;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Color(0xff54759E),
+                      borderRadius: BorderRadiusDirectional.circular(10),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        '+2',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      count -= 4;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Color(0xff54759E),
+                      borderRadius: BorderRadiusDirectional.circular(10),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        '-4',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      count += 4;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Color(0xff54759E),
+                      borderRadius: BorderRadiusDirectional.circular(10),
+                    ),
+                    child:const Center(
+                      child: Text(
+                        '+4',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  count = 0;
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Color(0xff54759E),
+                  borderRadius: BorderRadiusDirectional.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Clear',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+int count = 0;
